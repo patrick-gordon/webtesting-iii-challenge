edb1c1c89c8a1e78a29f9f12a71f0865976dba7e
Patrick Gordon <patrickgordon42@yahoo.com>
2019-08-19 22:39:24 -0500

    did test for controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,2 +1,21 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import {render} from '@testing-library/react';
+
+import Controls from './Controls'
+
+
+describe('<Controls /> initialState', () => {
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Controls />);
+        expect(tree.toJSON()).toMatchSnapshot();
+    })
+    it('gate lock status defaults to Unlocked', () => {
+        const { getByText } = render(<Controls />);
+        expect(getByText(/close gate/i)).toBeTruthy();
+      })
+      it('gate open status defaults to Open', () => {
+        const { getByText } = render(<Controls />);
+        expect(getByText(/lock gate/i)).toBeTruthy();
+      })
+})
